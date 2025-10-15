a8f8382ed3d2bf28dc949455f78315b672e685eb
KACPER LUKASZ GAJDA <kacper.gajda.2024@mumail.ie>
2025-10-15 11:47:33 +0100

    Added backend skeleton

---
+++ backend/server.js
@@ -0,0 +1,6 @@
+//import libraries needed
+const express = require("express");
+const app = express();
+const port = 3000; //common port if it doesnt work then just change
+const cors = require("cors");
+
