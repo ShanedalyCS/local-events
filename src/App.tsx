eec3e41f131f250d4106d62a7ce1124899a49dec
RUDOLF SOSE <rudolf.sose.2024@mumail.ie>
2025-11-19 12:11:57 +0000

    Edit App - Re-route to login page on the first webpage load

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -12,21 +12,20 @@ import Account from './Pages/Account.jsx';
 import LogIn from './Pages/LogIn.jsx';
 import Post from './Pages/Post.jsx';
 import Register from './Pages/Register.jsx';
-import Events from './Pages/Events.jsx';
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/account" element={<Account />} />
+          <Route path="/" element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />
+          <Route path="/about" element={<Account />} />
           <Route path="/login" element={<LogIn />} />
           <Route path="/post" element={<Post />} />
-           <Route path="/events" element={<Events/>} />
           <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>  
